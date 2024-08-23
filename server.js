d8b00700403dafb7960f718a0dc7d4a533e3a81f
Deepe <ossworupojha@gmail.com>
2024-08-23 18:25:40 +0530

    shift mongodb to online server

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express=require('express');
 const db=require('./db');
 const app=express();
+
+require('dotenv').config();
+const PORT=process.env.PORT || 3000;
+
 const bodyParser=require('body-parser');
 app.use(bodyParser.json());
 
@@ -14,6 +18,6 @@ app.use('/person',personRoute)
 const menuRoute=require('./routes/menuRoute');
 app.use('/menu',menuRoute);
 
-app.listen(3000,()=>{
+app.listen(PORT,()=>{
   console.log("listening on port 3000");
 });
